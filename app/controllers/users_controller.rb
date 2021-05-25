58b20a842cd759f10869d6b1b75e58da889a8792
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-05-25 17:38:18 -0400

    Add gravatar and user profile page with sidebar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  
   def new
+  end
+
+  def show
+  end
+
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
