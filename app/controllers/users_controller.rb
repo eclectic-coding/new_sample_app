14342118d365f41011671f4d9166eaacb4739249
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-05-25 17:52:36 -0400

    Style sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
-  
-  def new
-  end
 
   def show
   end
 
+  def new
+    @user = User.new
+  end
+
   private
-  
+
   def set_user
     @user = User.find(params[:id])
   end
