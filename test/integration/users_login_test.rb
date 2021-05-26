6d0bf1ca612429f145844b9b43c8a179b43867bf
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-05-26 11:00:56 -0400

    Finish testing log in feature

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,12 +18,21 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login in with valid information" do
     get login_path
-    post login_path, params: {session: {email: @user.email, password: 'password'}}
+    post login_path, params: {session: {email: @user.email, password: "password"}}
     assert_redirected_to @user
     follow_redirect!
     assert_template "users/show"
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+  end
+
+  test "login with valid email/invalid password" do
+    get login_path
+    post login_path, params: {session: {email: "user@example.com", password: "foo"}}
+    assert_template "sessions/new"
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
   end
 end
