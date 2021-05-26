25ebc0cf24c5586003b15cfc8ee70c4c30eeb626
Chuck <chuck@eclecticsaddlebag.com>
2021-05-26 17:42:55 -0400

    Chapter9 advanced login (#9)
    * Set up sessions routes
    * Design login form
    * Finish user log in workflow
    * Add user fixture and valid login test
    * Finish testing log in feature
    * Finish testing log in feature
    * Implement logout and test
    * Add remember me token logic and tests
    * Add remember me checkbox
    * Finish advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       reset_session
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       log_in user
-      flash[:success] = "Welcome to the Sample App!"
       redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
