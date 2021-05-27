bbc7f1917fcabaeabcde3e2e12bc8e3ab8221a36
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-05-27 07:49:44 -0400

    Add user delete action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @pagy, @users = pagy(User.all, items: 20)
@@ -39,6 +40,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
   def set_user
@@ -56,6 +63,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 
   def user_params
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,4 +43,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
+
+  test "should not allow the admin attribute to be editted via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: {password: "password",
+             password_confirmation: "password",
+             admin: false}
+    }
+  end
 end
